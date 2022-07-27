64233cc79b2b6c705e5f4668e5441c7be080e3c5
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-07-27 07:15:19 -0300

    Criado compomente navbar

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <>
+    <Navbar />
+  <h1>Olá Mundo</h1>
+  </>
   );
 }
 
